a60e02f3ed61e2633a79a893036978598ff43c54
Admin <2919681828@qq.com>
2022-07-11 11:09:53 +0800

    第一次修改代码 添加了一行语句

--- src/main/java/com/jd1901/AppDemo.java
+++ src/main/java/com/jd1901/AppDemo.java
@@ -3,5 +3,6 @@ package com.jd1901;
 public class AppDemo {
     public static void main(String[] args) {
         System.out.println("AppDemo.main");
+        System.out.println("第一次修改代码");
     }
 }
